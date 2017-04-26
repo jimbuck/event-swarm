e4c7f8ea190f16a4625a505a5086d796dcbae944
Jim Buck <jim@jimmyboh.com>
2017-04-25 22:09:10 -0400

    Simplified demo file.

--- src/demo.ts
+++ src/demo.ts
@@ -2,17 +2,8 @@ const readline = require('readline');
 import { EventSwarm, EventHandler, EventArgs } from './';
 import { Lookup } from './utils';
 
-interface ChatData {
-  from: string;
-  msg: string;
-}
-
-interface Introduction {
-  name: string;
-}
 
 let swarm: EventSwarm;
-let names: Lookup<string> = {};
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -20,63 +11,45 @@ const rl = readline.createInterface({
   prompt: `>`
 });
 
-rl.question(`What is your name?\n>`, (name: string) => {
-  rl.setPrompt(`${name}> `);
-  rl.on('close', async () => {
-    writeToChat('Have a great day!');
+rl.on('close', async () => {
+  await delay(200);
+  swarm.close();
 
-    await delay(200);
-    swarm.close();
-
-    await delay(200);
-    process.exit(0);
-  });
-  rl.question(`Which channel would you like to join?\n>`, (channel: string) => {
-
-    swarm = new EventSwarm({ channel });
-
-    swarm.on('event-swarm:connect', ({ id }) => {
-      writeToChat(`Connect (${id})`);
-    }).on('event-swarm:disconnect', ({ id }) => {
-      writeToChat(`"${name}" left the chat... (${id})`);
-    }).on<ChatData>('chat', (data, e) => {
-      writeToChat(data.from, data.msg);
-      rl.prompt(true);
-    }).on<Introduction>('intro', ({ name }, e) => {
-      names[e.sender] = name;
-      writeToChat(`"${name}" joined the chat...`);
-    });
-        
-    rl.on('line', (line: string) => {
-      line = line.trim();
-      if (line === '/exit') {
-        return rl.close();
-      }
-
-      swarm.emit<ChatData>('chat', { from: name, msg: line.trim() });
-      rl.prompt();
-    });
-
-    writeToChat(`Welcome to ${swarm.channel}... `);
-    setTimeout(() =>
-      swarm.emit<Introduction>('intro', { name })
-      , 100);
-
-    rl.prompt();
-  });
+  await delay(200);
+  process.exit(0);
 });
 
-function writeToChat(sender: string, msg?: string) {
-  if (!msg) {
-    msg = sender;
-    sender = '        ';
-  } else {
-    sender = `${sender}> `;
-  }
+rl.question(`Channel: `, (channel: string) => {
 
+  swarm = new EventSwarm({ channel });
+
+  swarm.on('event-swarm:connect', ({ id }) => {
+    writeToScreen(`Connect (${id})`);
+  }).on('event-swarm:disconnect', ({ id }) => {
+    writeToScreen(`Disconnect (${id})`);
+  }).on('message', ({ msg }, e) => {
+    writeToScreen(`${e.sender}: ${msg}`);
+  });
+        
+  rl.on('line', (line: string) => {
+    line = line.trim();
+    if (line === '/exit') {
+      return rl.close();
+    }
+
+    swarm.emit('message', { msg: line.trim() });
+    rl.prompt();
+  });
+
+  writeToScreen(`Joined "${swarm.channel}" as "${swarm.id}"...`);
+
+  rl.prompt();
+});
+
+function writeToScreen(msg: string) {
   readline.clearLine(process.stdout);
   readline.cursorTo(process.stdout, 0);
-  console.log(`${sender}${msg}`);
+  console.log(msg);
 }
 
 function delay(time: number): Promise<void> {
